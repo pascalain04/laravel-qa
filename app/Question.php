684b85b9fb6ccd1e3ad26543b26f52f57b360892
pascalain04 <pascalain04@gmail.com>
2019-11-19 16:12:50 +0100

    add author info and question date in question item

--- app/Question.php
+++ app/Question.php
@@ -16,5 +16,13 @@ class Question extends Model
         $this->attributes['slug']= Str_slug($value);
     }
 
+    public function getUrlAttribute(){
+        return route("questions.show", $this->id);
+    }
+
+    public function getCreatedDateAttribute(){
+        return $this->created_at->diffForHumans();
+    }
+
 
 }
